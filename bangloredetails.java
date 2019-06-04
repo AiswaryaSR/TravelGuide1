89a82e9bd8ac0a96a1cb45dcb9496d08f2fb2b47
AiswaryaSR <44803776+AiswaryaSR@users.noreply.github.com>
2019-06-04 22:30:12 +0530

    banglore

---
+++ bangloredetails.java
@@ -0,0 +1,15 @@
+package com.example.nyesteventure3.travelguide.details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nyesteventure3.travelguide.R;
+
+public class bangloredetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bangloredetails);
+    }
+}
